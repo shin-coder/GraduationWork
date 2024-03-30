a8b36ba732882b79098ccd37163063118573c560
shinnosuke <kawaguchi.shin.03@gmail.com>
2024-03-30 21:20:17 +0900

    MainViewセクション作成

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  loop: true, 
+  parallax: true,
+  allowTouchMove: false, 
+  speed: 1500, 
+  autoplay: { 
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+});
