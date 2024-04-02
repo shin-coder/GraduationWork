e93dd14f84791765fe735c708d501a2ca27d8a06
shinnosuke <kawaguchi.shin.03@gmail.com>
2024-04-02 22:02:52 +0900

    モバイル版のPhilosophyセクションとProductセクション（スライダー実装まで）完了

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const slider1 = new Swiper(".slider1", {
   loop: true, 
   parallax: true,
   allowTouchMove: false, 
@@ -7,4 +7,36 @@ const swiper = new Swiper(".swiper", {
     delay: 2000,
     disableOnInteraction: false,
   },
+});
+
+const slider2 = new Swiper('.slider2', {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' +
+        '<span class="slider-fraction-separator">/</span>' +
+        '<span class="' + totalClass + '"></span>';
+    },
+    formatFractionCurrent: function (number) {
+      if (number === 1){
+        return 4;
+      } else {
+        return number - 1;
+      }
+    },
+    formatFractionTotal: function (number) {
+      return 4;
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  centeredSlides: true,
+  initialSlide: 1, 
+  grabCursor: true,
+  loop: true,
 });
